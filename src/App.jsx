35bc98ea01e7afe0d7af44ced26c3c6a26adb0f0
Pedro Zuza <156633255+Pedrohenriquezuza@users.noreply.github.com>
2025-09-24 20:04:26 -0300

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+import { Header } from "./components/Header.jsx";
+const App = () => {
+  return (
+    <div className="w-full overflow-hidden">
+      <Header />
+    </div>
+  );
+};
+
+export default App;
